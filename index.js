c71bb03d56181f1e320877168b892778ec940291
Sungjun <wltjdwnsg@naver.com>
2022-05-19 02:26:51 +0900

    auth

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const {auth} = require('./middleware/auth');
+
 const cookieParser = require('cookie-parser')
 
 const bodyParser = require('body-parser')
@@ -27,7 +29,7 @@ app.get('/', (req, res) => {
   res.send('다시합니다..ㅠㅠ nodemon')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
 
     const user = new User(req.body)
@@ -41,7 +43,7 @@ app.post('/register', (req, res) => {
   })
   
 
-app.post('/login', (req,res) =>{
+app.post('/api/users/login', (req,res) =>{
 
 
     User.findOne({ email: req.body.email }, (err, user) => {
@@ -69,6 +71,22 @@ app.post('/login', (req,res) =>{
     })
   })
 
+app.get('/api/users/auth', auth ,(req,res) => {
+    app.get('/api/users/auth', auth, (req, res) => {
+        
+        res.status(200).json({
+          _id: req.user._id,
+          isAdmin: req.user.role === 0 ? false : true,
+          isAuth: true,
+          email: req.user.email,
+          name: req.user.name,
+          lastname: req.user.lastname,
+          role: req.user.role,
+          image: req.user.image
+        })
+      })
+      
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- models/User.js
+++ models/User.js
@@ -38,7 +38,7 @@ userSchema.pre('save', function( next ){
 
     var user = this;
     if (user.isModified('password')) {
-        //비밀번호를 암호화 시킨다.
+
         bcrypt.genSalt(saltRounds, function (err, salt) {
             if (err) return next(err)
 
@@ -64,13 +64,8 @@ userSchema.methods.comparePassword = function(plainPassword, cb) {
 
 userSchema.methods.generateToken = function (cb) {
     var user = this;
-    // console.log('user._id', user._id)
 
-    // jsonwebtoken을 이용해서 token을 생성하기 
     var token = jwt.sign(user._id.toHexString(), 'secretToken')
-    // user._id + 'secretToken' = token 
-    // -> 
-    // 'secretToken' -> user._id
 
     user.token = token
     user.save(function (err, user) {
@@ -81,7 +76,18 @@ userSchema.methods.generateToken = function (cb) {
 
 userSchema.statics.findByToken = function(token, cb) {
     var user = this;
-    // user._id + ''  = token
+
+    jwt.verify(token, 'secretToken', function (err, decoded) {
+
+        user.findOne({ "_id": decoded, "token": token }, function (err, user) {
+            if (err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
     //토큰을 decode 한다. 
     jwt.verify(token, 'secretToken', function (err, decoded) {
         //유저 아이디를 이용해서 유저를 찾은 다음에 
