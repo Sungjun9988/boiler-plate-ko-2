bab4a83eaf07f125d6fffc97cad65335f600f6b6
Sungjun <wltjdwnsg@naver.com>
2022-05-23 23:28:59 +0900

    logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,13 +1,28 @@
 import React, {useEffect} from 'react';
 import axios from 'axios';
-
+import { useNavigate } from 'react-router-dom';
 
 function LandingPage() {
+
+  let navigate = useNavigate();
 
     useEffect(() => {
         axios.get('/api/hello')
         .then(response => console.log(response.data))
     } ,[])
+
+
+const onClickHandler = () => {
+  axios.get('/api/users/logout')
+  .then(response => {
+    console.log(response.data)
+      if(response.data.success) {
+        navigate("/login")
+      } else {
+        alert('로그아웃 하는데 실패 했습니다')
+      }
+  })
+}
 
   return (
     <div style={{
@@ -15,6 +30,7 @@ function LandingPage() {
       width: '100%', height: '100vh'
     }}>
       <h2>시작 페이지</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   )
 }
